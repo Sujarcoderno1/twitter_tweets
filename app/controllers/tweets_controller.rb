7b85bc099cbb2e3944683317654faa160cc00b35
sumit <sumit.yuvasoft131@gmail.com>
2016-05-12 18:42:36 +0530

    Update user profile link

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,11 +3,13 @@ class TweetsController < ApplicationController
   def extract
     @url = params[:url]
     doc = Nokogiri::HTML(open(@url))
-    @user_profile = JSON.parse(doc.try(:children).try(:last).try(:elements).try(:first).try(:elements)[75].try(:values).try(:last))['profile_user']
     @tweets = doc.search('.content')
   end
 
   def index
   end
 
+  # def user_profile
+  #   @user_profile = JSON.parse(doc.try(:children).try(:last).try(:elements).try(:first).try(:elements)[75].try(:values).try(:last))['profile_user']
+  # end
 end
